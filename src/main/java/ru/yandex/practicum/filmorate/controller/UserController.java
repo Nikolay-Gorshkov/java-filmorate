8fa1cc2485314e5145b408c5c2696f4406bccd3e
Nikolay-Gorshkov <nik_gorshkov_2002@vk.com>
2025-02-20 21:38:50 +0300

    controllers-films-users sprint

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,51 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;  // Добавлен импорт
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+    private int idCounter = 0;
+
+    @PostMapping
+    public User createUser(@Valid @RequestBody User user) {
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        user.setId(++idCounter);
+        users.put(user.getId(), user);
+        log.info("Создан пользователь: {}", user);
+        return user;
+    }
+
+    @PutMapping
+    public User updateUser(@Valid @RequestBody User user) {
+        if (!users.containsKey(user.getId())) {
+            log.error("Пользователь с id {} не найден", user.getId());
+            throw new ValidationException("Пользователь с таким id не найден");
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        users.put(user.getId(), user);
+        log.info("Обновлен пользователь: {}", user);
+        return user;
+    }
+
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        log.info("Запрошен список всех пользователей");
+        return new ArrayList<>(users.values());
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private int id;
+
+    @NotBlank(message = "Email не может быть пустым")
+    @Email(message = "Некорректный формат email")
+    private String email;
+
+    @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "^\\S+$", message = "Логин не может содержать пробелы")
+    private String login;
+
+    private String name;
+
+    @NotNull(message = "Дата рождения обязательна")
+    @Past(message = "Дата рождения не может быть в будущем")
+    private LocalDate birthday;
+
+    public User() {}
+
+    public User(int id, String email, String login, String name, LocalDate birthday) {
+        this.id = id;
+        this.email = email;
+        this.login = login;
+        this.name = name;
+        this.birthday = birthday;
+    }
+}
