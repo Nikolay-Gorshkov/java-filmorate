138876b3218a174f15ab1dcb8b6ae3c68c9cd649
Nikolay-Gorshkov <nik_gorshkov_2002@vk.com>
2025-02-20 21:38:36 +0300

    controllers-films-users sprint

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,55 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int idCounter = 0;
+
+    @PostMapping
+    public Film addFilm(@Valid @RequestBody Film film) {
+        validateFilm(film);
+        film.setId(++idCounter);
+        films.put(film.getId(), film);
+        log.info("Добавлен фильм: {}", film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@Valid @RequestBody Film film) {
+        if (!films.containsKey(film.getId())) {
+            log.error("Фильм с id {} не найден", film.getId());
+            throw new ValidationException("Фильм с таким id не найден");
+        }
+        validateFilm(film);
+        films.put(film.getId(), film);
+        log.info("Обновлен фильм: {}", film);
+        return film;
+    }
+
+    @GetMapping
+    public List<Film> getAllFilms() {
+        log.info("Запрошен список всех фильмов");
+        return new ArrayList<>(films.values());
+    }
+
+    private void validateFilm(Film film) {
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.error("Дата релиза раньше 28 декабря 1895 года: {}", film.getReleaseDate());
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
+        }
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,34 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.*;
+import lombok.Data;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+@Data
 public class Film {
+    private int id;
+
+    @NotBlank(message = "Название фильма не может быть пустым")
+    private String name;
+
+    @Size(max = 200, message = "Описание не должно превышать 200 символов")
+    private String description;
+
+    @NotNull(message = "Дата релиза обязательна")
+    @PastOrPresent(message = "Дата релиза не может быть в будущем")
+    private LocalDate releaseDate;
+
+    @Positive(message = "Продолжительность фильма должна быть положительной")
+    private int duration;
+
+    public Film() {}
+
+    public Film(int id, String name, String description, LocalDate releaseDate, int duration) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+    }
 }
