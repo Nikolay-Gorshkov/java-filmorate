584e93362ac1fc47e10d98457796afe71e406e1a
Nikolay-Gorshkov <nik_gorshkov_2002@vk.com>
2025-03-31 13:58:55 +0300

    Спринт 11 chek style исправление

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FilmorateApplication {
-	public FilmorateApplication() {}
-
-	public static void main(final String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
 	}
 }
