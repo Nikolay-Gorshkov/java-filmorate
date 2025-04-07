b5095558ca82c684f96595e464231274a531dd7f
Nikolay-Gorshkov <nik_gorshkov_2002@vk.com>
2025-04-08 00:20:36 +0300

    sprint 12 check style

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -61,12 +61,15 @@ public class MpaController {
         public int getId() {
             return id;
         }
+
         public String getName() {
             return name;
         }
+
         public void setId(int id) {
             this.id = id;
         }
+
         public void setName(String name) {
             this.name = name;
         }
