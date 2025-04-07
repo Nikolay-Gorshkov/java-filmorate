ca77a3aab24624fd243ccd54f77b7c52dff3a7a7
Nikolay-Gorshkov <nik_gorshkov_2002@vk.com>
2025-04-08 00:36:04 +0300

    sprint 12 check style

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -16,7 +16,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @Import({UserDbStorage.class})
 class FilmorateApplicationTests {
-
 	private final UserDbStorage userStorage;
 
 	@Test
