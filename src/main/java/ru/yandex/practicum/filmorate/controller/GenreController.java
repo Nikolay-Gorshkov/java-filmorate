9d86071c9fbfa5afc4e79b58ae691f2cf49ce377
Nikolay-Gorshkov <nik_gorshkov_2002@vk.com>
2025-04-08 00:14:58 +0300

    sprint 12 check style

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -60,17 +60,12 @@ public class GenreController {
             this.name = name;
         }
 
-        public int getId() {
-            return id;
-        }
-        public String getName() {
-            return name;
-        }
-        public void setId(int id) {
-            this.id = id;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
+        public int getId() { return id; }
+
+        public String getName() { return name; }
+
+        public void setId(int id) { this.id = id; }
+
+        public void setName(String name) { this.name = name; }
     }
 }
