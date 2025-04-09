28ec00233956d62f7e4008d6afa5993d8c79c62f
Nikolay-Gorshkov <nik_gorshkov_2002@vk.com>
2025-04-09 12:36:40 +0300

    sprint 12 правки

--- src/main/java/ru/yandex/practicum/filmorate/storage/Mpa/MpaStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/Mpa/MpaStorage.java
@@ -4,7 +4,9 @@ import ru.yandex.practicum.filmorate.model.MpaaRating;
 import java.util.List;
 
 public interface MpaStorage {
+
     List<MpaaRating> getAllMpa();
+
     MpaaRating getMpaById(int id);
 }
 
