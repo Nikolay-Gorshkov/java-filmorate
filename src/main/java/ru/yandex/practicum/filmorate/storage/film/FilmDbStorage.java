46d8a55a9208c9329c190b1818aeffbc682a5db6
Ivan G <grikoivan@yandex.ru>
2025-05-08 23:11:40 +0300

    Общие фильмы.

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -16,7 +16,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 @Repository("filmDbStorage")
 public class FilmDbStorage implements FilmStorage {
