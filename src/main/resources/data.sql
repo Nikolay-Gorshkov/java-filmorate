698eda91c206996ca6fab37ff2a0b4701bf01b3a
Ivan G <grikoivan@yandex.ru>
2025-05-14 22:51:46 +0300

    добавил жанры

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,52 @@ WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = 'user2@example.com');
 INSERT INTO users (email, login, name, birthday)
 SELECT 'user3@example.com', 'user3', 'User Three', '1995-03-03'
 WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = 'user3@example.com');
+
+INSERT INTO genres (name)
+SELECT 'Комедия'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres
+    WHERE name = 'Комедия'
+);
+
+INSERT INTO genres (name)
+SELECT 'Драма'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres
+    WHERE name = 'Драма'
+);
+
+INSERT INTO genres (name)
+SELECT 'Мультфильм'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres
+    WHERE name = 'Мультфильм'
+);
+
+INSERT INTO genres (name)
+SELECT 'Триллер'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres
+    WHERE name = 'Триллер'
+);
+
+INSERT INTO genres (name)
+SELECT 'Документальный'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres
+    WHERE name = 'Документальный'
+);
+
+INSERT INTO genres (name)
+SELECT 'Боевик'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM genres
+    WHERE name = 'Боевик'
+);
+
