c4cd75b7e316a58f5dc04602c707995140758f41
Nikolay-Gorshkov <nik_gorshkov_2002@vk.com>
2025-05-16 16:48:28 +0300

    sprint 13 develop-ref work стабилизация

---
+++ src/main/resources/test-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (email, login, name, birthday)
+VALUES ('test@example.com', 'test', 'Test User', '1990-01-01');
--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 
@@ -19,6 +20,7 @@ class FilmorateApplicationTests {
     private final UserDbStorage userStorage;
 
     @Test
+    @Sql("/test-data.sql")
     public void testFindUserById() {
         User user = userStorage.getUserById(1);
 
